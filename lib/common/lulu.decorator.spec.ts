41fcac25cf9fa1ad602cec4b39181c3277df25f8
Dexter Hardy <dhardy@HQPro.local>
2020-03-31 22:05:34 -0400

    u: added license, testing client.init

---
+++ lib/common/lulu.decorator.spec.ts
@@ -0,0 +1,37 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { LuluConfigOptions, LuluService } from '@ntegral/lulu';
+import { Injectable } from '@nestjs/common';
+import { InjectLulu } from './lulu.decorator';
+import { LuluModule } from '../lulu.module';
+
+describe('InjectS3', () => {
+
+    let config: LuluConfigOptions = {
+        client_key: '--- lulu client key ---',
+        client_secret: '--- lulu client secret ---',
+        environment: 'development'
+    };
+
+    let module: TestingModule;
+
+    @Injectable()
+    class InjectableService {
+        public constructor(@InjectLulu() public readonly client: LuluService) {}
+    }
+
+    beforeEach(async () => {
+        module = await Test.createTestingModule({
+            imports: [LuluModule.forRoot(config)],
+            providers: [InjectableService],
+        }).compile();
+    });
+
+    describe('when decorating a class constructor parameter', () => {
+        it('should inject the lulu client', () => {
+            const testService = module.get(InjectableService);
+            expect(testService).toHaveProperty('client');
+            expect(testService.client).toBeInstanceOf(LuluService);
+        });
+    });
+})
---
+++ lib/lulu.module.spec.ts
@@ -0,0 +1,70 @@
+import { Test } from '@nestjs/testing';
+
+import { LuluModule } from './lulu.module';
+import { LULU_TOKEN } from './common/lulu.constants';
+import { LuluConfigOptions, LuluService } from '@ntegral/lulu';
+import { LuluConfigOptionsFactory } from './interfaces';
+
+describe('LuluModule', () => {
+
+    let config: LuluConfigOptions = {
+        client_key: '--- lulu client key ---',
+        client_secret: '--- lulu client secret ---',
+        environment: 'development'
+    };
+
+    class TestService implements LuluConfigOptionsFactory {
+        createLuluOptions(): LuluConfigOptions {
+            return config;
+        }
+    }
+
+    describe('forRoot', () => {
+        it('should provide the lulu client', async() => {
+            const mod = await Test.createTestingModule({
+                imports: [LuluModule.forRoot(config)],
+            }).compile();
+
+            const client = mod.get<LuluService>(LULU_TOKEN);
+            await client.init();
+            console.log('lulu', client);
+            // expect(client).resolves.toBeDefined();
+            expect(client).toBeInstanceOf(LuluService);
+        });
+    });
+
+    describe('forRootAsync', () => {
+        describe('when the `useFactory` option is used', () => {
+            it('should provide lulu client', async () => {
+                const mod = await Test.createTestingModule({
+                    imports: [
+                        LuluModule.forRootAsync({
+                            useFactory: () => (config),
+                        }),
+                    ]
+                }).compile();
+
+                const client = mod.get<LuluService>(LULU_TOKEN);
+                await client.init();
+                expect(client).toBeDefined();
+                expect(client).toBeInstanceOf(LuluService);
+            });
+        })
+    });
+
+    describe('when the `useClass` option is used', () => {
+        it('should provide the lulu client', async () => {
+            const mod = await Test.createTestingModule({
+                imports: [
+                    LuluModule.forRootAsync({
+                        useClass: TestService
+                    })
+                ]
+            }).compile();
+
+            const client = mod.get<LuluService>(LULU_TOKEN);
+            expect(client).toBeDefined();
+            expect(client).toBeInstanceOf(LuluService);
+        });
+    });
+})
